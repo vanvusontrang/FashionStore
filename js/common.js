9b6d82d7c3f0dbaa155cac547bf759758ac29c06
vanvusontrang <phan.chinhbk@gmail.com>
2021-12-27 15:17:38 +0700

    tools navbar event

--- js/common.js
+++ js/common.js
@@ -4,11 +4,33 @@ $(document).ready(function(){
     function scrollFunction() {
         if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             $("#tool-navbar").css("top", "0px");
-            $("#left-navbar").css("top", "54px");
+            $("#left-navbar").css("top", "40px");
         }
         else {
             $("#tool-navbar").css("top", "80px");
-            $("#left-navbar").css("top", "134px");
+            $("#left-navbar").css("top", "120px");
         }
     }
+    // tools navbar click event
+    $("#clothes").click(function() {
+        $("#clothes").addClass("active");
+        $("#dress").removeClass("active");
+        $("#shoes").removeClass("active");
+
+        $("#navbar-text").text("CLOTHES");
+    });
+    $("#dress").click(function() {
+        $("#dress").addClass("active");
+        $("#clothes").removeClass("active");
+        $("#shoes").removeClass("active");
+
+        $("#navbar-text").text("DRESS");
+    });
+    $("#shoes").click(function() {
+        $("#shoes").addClass("active");
+        $("#clothes").removeClass("active");
+        $("#dress").removeClass("active");
+
+        $("#navbar-text").text("SHOES");
+    });
 });
